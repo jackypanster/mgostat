ca7664c7dc23668fdb8919cb68d812d3b45d696e
jackypan <87015598@qq.com>
2017-12-05 15:10:18 +0800

    init project

--- script.js
+++ script.js
@@ -1,15 +1,37 @@
-db.messages.aggregate([
-    {
-        $match:
-            {
-                'date':'11/16/2017'
-            }
-    },
-    {
-        $group:
-            {
-                _id: {'status':'$status', 'date':'$date'},
-                total: { $sum: 1 }
-            }
-    }
-])
+var dates = ['11/13/2017', '11/14/2017', '11/15/2017', '11/16/2017', '11/17/2017'];
+
+for (var i = 0; i < dates.length; i++) {
+    printjson(db.batches.aggregate([
+        {
+            $match:
+                {
+                    'date': dates[i]
+                }
+        },
+        {
+            $group:
+                {
+                    _id: {'path': '$path', 'status': '$status', 'date': '$date'},
+                    total: {$sum: '$count'}
+                }
+        }
+    ]));
+}
+
+for (i = 0; i < dates.length; i++) {
+    printjson(db.messages.aggregate([
+        {
+            $match:
+                {
+                    'date': dates[i]
+                }
+        },
+        {
+            $group:
+                {
+                    _id: {'status': '$status', 'date': '$date'},
+                    total: {$sum: 1}
+                }
+        }
+    ]));
+}
