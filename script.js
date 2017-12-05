fe8b47dad696821db7b5079846a73d596db157c4
jackypan <87015598@qq.com>
2017-12-05 15:25:35 +0800

    init project

--- script.js
+++ script.js
@@ -1,42 +1,39 @@
 use dolphin;
-db.messages.find();
 
+var dates = ['11/13/2017', '11/14/2017', '11/15/2017', '11/16/2017', '11/17/2017'];
 
-    /*var dates = ['11/13/2017', '11/14/2017', '11/15/2017', '11/16/2017', '11/17/2017'];
+for (var i = 0; i < dates.length; i++) {
+    db.batches.aggregate([
+        {
+            $match:
+                {
+                    'date': dates[i]
+                }
+        },
+        {
+            $group:
+                {
+                    _id: {'path': '$path', 'status': '$status', 'date': '$date'},
+                    total: {$sum: '$count'}
+                }
+        }
+    ])
+}
 
-
-    for (var i = 0; i < dates.length; i++) {
-        printjson(db.batches.aggregate([
-            {
-                $match:
-                    {
-                        'date': dates[i]
-                    }
-            },
-            {
-                $group:
-                    {
-                        _id: {'path': '$path', 'status': '$status', 'date': '$date'},
-                        total: {$sum: '$count'}
-                    }
-            }
-        ]));
-    }
-
-    for (i = 0; i < dates.length; i++) {
-        printjson(db.messages.aggregate([
-            {
-                $match:
-                    {
-                        'date': dates[i]
-                    }
-            },
-            {
-                $group:
-                    {
-                        _id: {'status': '$status', 'date': '$date'},
-                        total: {$sum: 1}
-                    }
-            }
-        ]));
-    }*/
+for (i = 0; i < dates.length; i++) {
+    db.messages.aggregate([
+        {
+            $match:
+                {
+                    'date': dates[i]
+                }
+        },
+        {
+            $group:
+                {
+                    _id: {'status': '$status', 'date': '$date'},
+                    total: {$sum: 1}
+                }
+        }
+    ])
+}
