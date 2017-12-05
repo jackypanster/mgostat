ad1f7268ca7e6b70c861065c125179a096870041
jackypan <87015598@qq.com>
2017-12-05 15:20:15 +0800

    init project

--- script.js
+++ script.js
@@ -1,39 +1,43 @@
-var dates = ['11/13/2017', '11/14/2017', '11/15/2017', '11/16/2017', '11/17/2017'];
+function start() {
+    return db.messages.find();
 
-printjson(db.messages.find());
 
-for (var i = 0; i < dates.length; i++) {
-    printjson(db.batches.aggregate([
-        {
-            $match:
-                {
-                    'date': dates[i]
-                }
-        },
-        {
-            $group:
-                {
-                    _id: {'path': '$path', 'status': '$status', 'date': '$date'},
-                    total: {$sum: '$count'}
-                }
-        }
-    ]));
-}
+    /*var dates = ['11/13/2017', '11/14/2017', '11/15/2017', '11/16/2017', '11/17/2017'];
 
-for (i = 0; i < dates.length; i++) {
-    printjson(db.messages.aggregate([
-        {
-            $match:
-                {
-                    'date': dates[i]
-                }
-        },
-        {
-            $group:
-                {
-                    _id: {'status': '$status', 'date': '$date'},
-                    total: {$sum: 1}
-                }
-        }
-    ]));
+
+    for (var i = 0; i < dates.length; i++) {
+        printjson(db.batches.aggregate([
+            {
+                $match:
+                    {
+                        'date': dates[i]
+                    }
+            },
+            {
+                $group:
+                    {
+                        _id: {'path': '$path', 'status': '$status', 'date': '$date'},
+                        total: {$sum: '$count'}
+                    }
+            }
+        ]));
+    }
+
+    for (i = 0; i < dates.length; i++) {
+        printjson(db.messages.aggregate([
+            {
+                $match:
+                    {
+                        'date': dates[i]
+                    }
+            },
+            {
+                $group:
+                    {
+                        _id: {'status': '$status', 'date': '$date'},
+                        total: {$sum: 1}
+                    }
+            }
+        ]));
+    }*/
 }
